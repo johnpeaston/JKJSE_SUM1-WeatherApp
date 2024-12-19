00959ec9874daefd779ae212b20ed265615f2c19
Mikhaela-Paige <65867690+Mikhaela-Paige@users.noreply.github.com>
2024-12-19 13:28:33 +0000

    Create Initial Frontend

--- index.js
+++ index.js
@@ -12,8 +12,27 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// test.js
-declareEndpoint(app);
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/local-weather", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL, {
+      params: {
+        lat: req.query.latitude,
+        lon: req.query.longitude,
+        appid: apiKey,
+      },
+    });
+    console.log(result);
+    res.render("index.ejs", {
+      weather: JSON.stringify(result.data),
+    });
+  } catch (error) {
+    res.render("index.ejs", { weather: JSON.stringify(error.response) });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
